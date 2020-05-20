832eb6a2e1e1a8be736c16389a5798dc877d8f09
Jakub Biesiada <biesiadajakub@icloud.com>
2020-05-20 17:27:22 +0200

    chore: include missing components

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import 'figma-plugin-ds/dist/figma-plugin-ds.css';
 
 export { Button } from './components/Button';
 export { Checkbox } from './components/Checkbox';
-export { Disclosure } from './components/Disclosure';
+export * from './components/Disclosure';
 export { Icon } from './components/Icon';
 export { IconButton } from './components/IconButton';
 export { Input } from './components/Input';
@@ -10,7 +10,7 @@ export { Label } from './components/Label';
 export { Onboarding } from './components/Onboarding';
 export { Radio } from './components/Radio';
 export { SectionTitle } from './components/SectionTitle';
-export { SelectMenu } from './components/SelectMenu';
+export * from './components/SelectMenu';
 export { Switch } from './components/Switch';
 export { Textarea } from './components/Textarea';
 // export { Type } from './components/Type';
