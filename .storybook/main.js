9e602b8f31124066a7e0a874c3759b6ee5c379fe
Jakub Biesiada <biesiadajakub@icloud.com>
2020-07-19 07:34:37 +0200

    feat: add code preview to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
-  addons: ['@storybook/addon-links', '@storybook/addon-knobs'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-knobs',
+    '@storybook/addon-storysource',
+  ],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
