0590a76535421e8eb6a84c8efe7d409c822318d6
Jakub Biesiada <biesiadajakub@icloud.com>
2021-02-24 22:35:57 +0100

    test: update tests for Label, SectionTitle, Type

--- __tests__/SectionTitle.test.tsx
+++ __tests__/SectionTitle.test.tsx
@@ -5,15 +5,23 @@ import { SectionTitle } from '../src';
 
 describe('SectionTitle', () => {
   it('should render Section title', () => {
-    const { container } = render(<SectionTitle>Section title</SectionTitle>);
+    const { getByText, container } = render(
+      <SectionTitle>Section title</SectionTitle>
+    );
+
+    expect(getByText('Section title')).toBeInTheDocument();
 
     expect(container).toMatchSnapshot();
   });
 
   it('should render Section title with custom className', () => {
-    const { container } = render(
-      <SectionTitle className="custom-class-name">Section title</SectionTitle>
+    const { getByText, container } = render(
+      <SectionTitle className="custom-class-name">
+        Another section title
+      </SectionTitle>
     );
+
+    expect(getByText('Another section title')).toBeInTheDocument();
 
     expect(container).toMatchSnapshot();
   });
