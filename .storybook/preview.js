147f4243268be847a98976fbc96f60067b20629a
Jakub Biesiada <biesiadajakub@icloud.com>
2021-03-10 09:06:28 +0100

    chore: update storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -23,7 +23,6 @@ addDecorator((Story) => (
       padding: '1rem',
     }}
   >
-    {/* TODO */}
     {/* TODO remove temporary workaround - https://github.com/storybookjs/storybook/issues/11657 */}
     {Story()}
     {/* <Story /> */}
