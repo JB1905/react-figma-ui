dc7db965b3ddce1be0aa9a9241a73d91b105bd79
Jakub Biesiada <biesiadajakub@icloud.com>
2021-03-28 21:44:42 +0200

    fix: update type

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,8 +4,8 @@ import { sizes } from '../constants/sizes';
 import { tints } from '../constants/tints';
 import { weights } from '../constants/weights';
 
-export type ColorName = typeof colorNames;
-export type IconName = typeof iconNames;
-export type Size = typeof sizes;
-export type Tint = typeof tints;
-export type Weight = typeof weights;
+export type ColorName = typeof colorNames[number];
+export type IconName = typeof iconNames[number];
+export type Size = typeof sizes[number];
+export type Tint = typeof tints[number];
+export type Weight = typeof weights[number];
